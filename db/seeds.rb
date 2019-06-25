455ff94ab4b90d5bbb56ced5b11dbcca1c523415
shane <smp1993@gmail.com>
2019-06-25 19:56:50 -0400

    Created 3 instances of Product class, backpack, wig, and sweatshirt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+wig = Product.new(name: "wig", price: 20, image_url: "https://partycity6.scene7.com/is/image/PartyCity/_sq_?$_500x500_$&$product=PartyCity/793219_full", description: "a beautiful wig!!")
+wig.save
+
+sweatshirt = Product.new(name: "sweatshirt", price: 55, image_url: "https://i.ebayimg.com/images/g/Y8kAAOSwKUhZtlXO/s-l300.jpg", description: "a cool sweatshirt with a wolf")
+
+sweatshirt.save
