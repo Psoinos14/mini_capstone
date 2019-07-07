358283576de1133d96d4152c6297da93f354703b
shane <smp1993@gmail.com>
2019-07-07 13:31:23 -0400

    added searchable by discount active record

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,6 +3,8 @@ class Api::ProductsController < ApplicationController
   def index
     if params[:search]
       @products = Product.where("name LIKE ?", "%#{params[:search]}%")
+    elsif params[:discount]
+      @products = Product.where("price < ?", "#{params[:discount]}")
     else
       @products = Product.all
     end
